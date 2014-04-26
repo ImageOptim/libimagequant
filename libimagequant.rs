5abb17b3959875c448ce751a613d8040e39f7436
Kornel Lesiński <kornel@geekhood.net>
2014-04-26 23:07:49 +0100

    Fixed Color struct

--- libimagequant.rs
+++ libimagequant.rs
@@ -12,7 +12,10 @@ use std::vec::Vec;
 use std::fmt;
 
 pub struct Color {
-    r: u8, g: u8, b: u8, a: u8,
+    pub r: u8,
+    pub g: u8,
+    pub b: u8,
+    pub a: u8,
 }
 
 impl fmt::Show for Color {
