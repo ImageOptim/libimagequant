d54ae148ec871801ff996d9eb9a28b3046f9c4c6
Kornel Lesiński <kornel@geekhood.net>
2015-06-12 22:51:17 +0100

    Upgraded to 2.4.1

--- build.rs
+++ build.rs
@@ -1,5 +1,5 @@
 fn main() {
-    if !std::process::Command::new("make").status().unwrap().success() {
+    if !std::process::Command::new("make").arg("-j8").status().unwrap().success() {
         panic!("Script failed");
     }
     println!("cargo:rustc-flags=-L pngquant-2.3.5/lib");
