17230a3f0949b3887850c61356c4480a0a1022c3
Kornel Lesiński <kornel@geekhood.net>
2015-08-10 22:44:38 +0100

    Version bump

--- build.rs
+++ build.rs
@@ -2,5 +2,5 @@ fn main() {
     if !std::process::Command::new("make").arg("-j8").status().unwrap().success() {
         panic!("Script failed");
     }
-    println!("cargo:rustc-flags=-L pngquant-2.3.5/lib");
+    println!("cargo:rustc-flags=-L pngquant-2.5.1/lib");
 }
