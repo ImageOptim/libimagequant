9859578b5b1b12205a8aad66b8cb4357fcf49554
Kornel Lesiński <kornel@geekhood.net>
2016-12-01 13:09:49 +0000

    2.8.1

--- libimagequant.h
+++ libimagequant.h
@@ -13,8 +13,8 @@
 #define LIQ_EXPORT extern
 #endif
 
-#define LIQ_VERSION 20800
-#define LIQ_VERSION_STRING "2.8.0"
+#define LIQ_VERSION 20801
+#define LIQ_VERSION_STRING "2.8.1"
 
 #ifndef LIQ_PRIVATE
 #if defined(__GNUC__) || defined (__llvm__)
