5f678094647988420a1f2459df774864018909cf
Kornel <kornel@geekhood.net>
2017-05-03 18:40:59 +0100

    Use gcc crate to build

--- Makefile
+++ Makefile
@@ -1,15 +1,14 @@
 OUT_DIR?=vendor
 LIQDIR=$(OUT_DIR)/lib
 
-all: $(LIQDIR)/libimagequant.a
-
-$(LIQDIR)/libimagequant.a:: $(LIQDIR)
-	make -C $(LIQDIR) static
+all: $(LIQDIR)
 
 $(LIQDIR):
-	curl -L https://pngquant.org/pngquant-2.9.0-src.tar.gz | tar xz -C $(OUT_DIR) --strip-components=1
+	mkdir -p "$(OUT_DIR)"
+	curl -L https://pngquant.org/pngquant-2.9.1-src.tar.gz | tar xz -C "$(OUT_DIR)" --strip-components=1 '*/lib'
 
 clean:
 	make -C $(LIQDIR) clean
 
 .PHONY: all clean
+.DELETE_ON_ERROR:
--- build.rs
+++ build.rs
@@ -1,6 +1,22 @@
+extern crate gcc;
+use std::env;
+use std::path::PathBuf;
+
 fn main() {
-    if !std::process::Command::new("make").arg("-j8").status().unwrap().success() {
-        panic!("Script failed");
+    if !std::process::Command::new("make").status().unwrap().success() {
+        panic!("Download failed");
     }
-    println!("cargo:rustc-flags=-L {}/lib", std::env::var("OUT_DIR").unwrap());
+
+    let mut out_dir: PathBuf = env::var_os("OUT_DIR").unwrap().into();
+    out_dir.push("lib");
+
+    gcc::compile_library("libimagequant.a", &[
+        &out_dir.join("blur.c").to_str().unwrap(),
+        &out_dir.join("kmeans.c").to_str().unwrap(),
+        &out_dir.join("libimagequant.c").to_str().unwrap(),
+        &out_dir.join("mediancut.c").to_str().unwrap(),
+        &out_dir.join("mempool.c").to_str().unwrap(),
+        &out_dir.join("nearest.c").to_str().unwrap(),
+        &out_dir.join("pam.c").to_str().unwrap(),
+    ]);
 }
