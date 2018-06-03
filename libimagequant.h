715d99a756858c78d74fe351a9ef24838e855f5c
Kornel <kornel@geekhood.net>
2018-06-03 15:23:41 +0100

    Bump

--- libimagequant.h
+++ libimagequant.h
@@ -13,8 +13,8 @@
 #define LIQ_EXPORT extern
 #endif
 
-#define LIQ_VERSION 21111
-#define LIQ_VERSION_STRING "2.11.11"
+#define LIQ_VERSION 21200
+#define LIQ_VERSION_STRING "2.12.0"
 
 #ifndef LIQ_PRIVATE
 #if defined(__GNUC__) || defined (__llvm__)
