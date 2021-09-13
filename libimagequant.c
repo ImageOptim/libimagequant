26af56ed7ffbec97640c52145137a5f41ec8213b
Joshua Sager <joshua.sager@shopify.com>
2021-09-13 17:50:33 -0400

    Initialize rows using heap for large images

--- libimagequant.c
+++ libimagequant.c
@@ -2100,7 +2100,7 @@ LIQ_EXPORT LIQ_NONNULL liq_error liq_write_remapped_image(liq_result *result, li
         return LIQ_BUFFER_TOO_SMALL;
     }
 
-    LIQ_ARRAY(unsigned char *, rows, input_image->height);
+    unsigned char **rows = malloc(input_image->height * sizeof(unsigned char *));
     unsigned char *buffer_bytes = buffer;
     for(unsigned int i=0; i < input_image->height; i++) {
         rows[i] = &buffer_bytes[input_image->width * i];
