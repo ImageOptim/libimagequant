399f14cc95190e7125779a1819a264047c5f8ef0
Kornel <kornel@geekhood.net>
2025-07-29 13:52:49 +0100

    CI fix

--- imagequant-sys/build.rs
+++ imagequant-sys/build.rs
@@ -1,6 +1,6 @@
 fn main() {
     if cfg!(all(feature = "std", feature = "no_std")) {
-        println!("cargo::warning=both std and no_std features are enabled in imagequant-sys");
+        println!("cargo:warning=both std and no_std features are enabled in imagequant-sys");
     }
     println!("cargo:include={}", std::env::var("CARGO_MANIFEST_DIR").unwrap());
 }
