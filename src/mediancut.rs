938cf5f627e6eed5fdbce80e1d92c1bb8fb73ca3
Lilith River <lilith@imazen.io>
2026-01-18 03:23:39 -0700

    Remove HistSortTmp union in favor of plain u32
    The union was used to reinterpret the same memory as either mc_sort_value
    (u32) during median cut sorting, or likely_palette_index (PalIndex) after.
    
    Since PalIndex fits in u32, we can simply store u32 and cast on read.
    This eliminates unsafe union field access with no runtime cost.

--- src/mediancut.rs
+++ src/mediancut.rs
@@ -96,7 +96,7 @@ impl<'hist> MBox<'hist> {
             let chans: [f32; 4] = rgb::bytemuck::cast(item.color.0);
             // Only the first channel really matters. But other channels are included, because when trying median cut
             // many times with different histogram weights, I don't want sort randomness to influence the outcome.
-            item.tmp.mc_sort_value = (u32::from((chans[channels[0].chan] * 65535.) as u16) << 16)
+            item.tmp = (u32::from((chans[channels[0].chan] * 65535.) as u16) << 16)
                 | u32::from(((chans[channels[2].chan] + chans[channels[1].chan] / 2. + chans[channels[3].chan] / 4.) * 65535.) as u16); // box will be split to make color_weight of each side even
         }
     }
@@ -264,7 +264,7 @@ impl<'hist> MedianCutter<'hist> {
         let mut palette = PalF::new();
 
         for (i, mbox) in self.boxes.iter_mut().enumerate() {
-            mbox.colors.iter_mut().for_each(move |a| a.tmp.likely_palette_index = i as _);
+            mbox.colors.iter_mut().for_each(move |a| a.tmp = i as _);
 
             // store total color popularity (perceptual_weight is approximation of it)
             let pop = mbox.colors.iter().map(|a| f64::from(a.perceptual_weight)).sum::<f64>();
